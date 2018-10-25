ee79f17addcc0dd9cd7f23e17468ba195a761203
ROOT\vigneshkumar.ramasam <vigneshkumar.ramasamy@syncfusion.com>
2018-10-25 15:22:55 +0530

    SfDataGrid sample committed.

---
+++ SfDataGrid/SfDataGridSample/SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Syncfusion.SfDataGrid.XForms;
+using Syncfusion.SfDataGrid.XForms.Renderers;
+using Syncfusion.SfNumericTextBox.XForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            sequenceOrderGrid.GridStyle = new CustomStyle();
+        }
+    }
+
+    public class CustomStyle : DataGridStyle
+    {
+        public override GridLinesVisibility GetGridLinesVisibility()
+        {
+            return GridLinesVisibility.None;
+        }
+    }
+}
+    
+
+    
---
+++ SfDataGrid/SfDataGridSample/SfDataGridSample/ViewModel/ViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SfDataGridSample
+{
+    public class ViewModel : NotificationObject
+    {
+        OrderInfoRepository order;
+        public ViewModel()
+        {
+            SetRowstoGenerate(100);
+        }
+
+        #region ItemsSource
+
+        private ObservableCollection<OrderInfo> ordersInfo;
+        public ObservableCollection<OrderInfo> OrdersInfo
+        {
+            get { return ordersInfo; }
+            set { this.ordersInfo = value; }
+        }
+
+        #endregion
+
+        #region ItemSource Generator
+
+        public void SetRowstoGenerate(int count)
+        {
+            order = new OrderInfoRepository();
+            ordersInfo = order.GetOrderDetails(count);
+        }
+
+        #endregion
+    }
+
+    public class NotificationObject : INotifyPropertyChanged
+    {
+        public void RaisePropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
